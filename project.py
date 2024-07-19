719fd94aeb5f200bc1a62b14b148bf7b69305bee
Xavier Evans <137194137+Iam-Default@users.noreply.github.com>
2024-07-19 18:33:14 +0300

    rev 1

---
+++ project.py
@@ -0,0 +1,20 @@
+import time
+import random
+
+passlist = ["Apple@123", "12345678", "p@$$word"]
+
+def print_pause(phrases):
+    for i in phrases:
+        print(i)
+        time.sleep(1)
+
+def game_init():
+    score = 0
+    password = random.choice(passlist)
+
+def lobby():
+    print_pause("You are an ")
+    
+print_pause(["hi",
+             ("hello" + " basel"),
+             "ccc"])
